475bc1beb8fd9602eb61327f443e84945e3b4b68
PerseP <persep@gmail.com>
2014-08-30 18:32:26 +0200

    Add delete

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,6 @@
 class Article < ActiveRecord::Base
-	has_many :comments # One article can have many comments
+	has_many :comments, # One article can have many comments
+		dependent: :destroy # when you delete an article you delete all the comments
 	
 	validates :title, presence: true, length: { minimum: 5 }
 end
